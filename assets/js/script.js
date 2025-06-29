92e72f6256b7d90ba175dd360e3203e60a0fd5b5
yash <yporwal333@gmail.com>
2025-06-29 10:25:21 +0530

    yp: hero-section and clients section design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 $(function () {
+  // Hamburger button toggle js
   $(".hamburger button").on("click", function () {
     $(this).toggleClass("show");
   });
 
+  //   Header menu toggle js
   $(".hamburger button").on("click", function () {
     $(".header .center").slideToggle(500);
   });
+  //   Hero sldier js
+  new Splide(".hero-slider", {
+    type: "loop",
+    perPage: 1,
+    autoplay: false,
+    pauseOnHover: true,
+    arrows: false,
+  }).mount();
+
+
 });
